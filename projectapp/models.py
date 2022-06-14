8dfb54177a7e65ab77f4c13a1d34ec71fe73fd06
CJ0823 <changjun0823@gmail.com>
2022-06-14 17:53:33 +0900

    ch.45 WYSIWYG, django 프로젝트 마무리

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=20, null=False)
@@ -7,3 +8,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False)
 
     created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.pk} : {self.title}'
