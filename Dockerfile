76bc4c52f8f9e2121a8a81f28b1deb39831e2b65
CJ0823 <changjun0823@gmail.com>
2022-06-24 07:00:16 +0900

    docker secrets 적용

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,20 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
+RUN echo "testing1234"
+
 RUN git clone https://github.com/CJ0823/inflearn_study_django_pinterest.git
 
 WORKDIR /home/inflearn_study_django_pinterest/
 
 RUN pip install -r requirements.txt
 
-RUN echo "SECRET_KEY=django-insecure-$ioa*(%5%p#1&c(as*nqzw7xpo(g8l6ovb_&d+x-+(-pdyo&z+" > .env
+RUN pip install gunicorn
 
-RUN python manage.py migrate
+RUN pip install mysqlclient
+
+RUN echo "SECRET_KEY=django-insecure-$ioa*(%5%p#1&c(as*nqzw7xpo(g8l6ovb_&d+x-+(-pdyo&z+" > .env
 
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["bash", "-c", "python manage.py collectstatic --noinput && python manage.py migrate --settings=pragmatic.settings.deploy && gunicorn pragmatic.wsgi --env DJANGO_SETTINGS_MODULE=pragmatic.settings.deploy --bind 0.0.0.0:8000"]
--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -14,6 +14,13 @@ from .base import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
@@ -21,7 +28,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
